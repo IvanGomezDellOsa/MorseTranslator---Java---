8f0fb010b5e88316f3f4b881fe027cf5d864e8bf
Ivan Gomez DellOsa <ivangomezdellosa@gmail.com>
2024-04-02 06:57:42 -0300

    MorseConverter completo

---
+++ Readme.md
@@ -0,0 +1,37 @@
+﻿Este es un programa que he creado con el objetivo de convertir palabras entre el alfabeto y el código Morse, y viceversa.
+
+Estructura del código:
+
+He estructurado el código en diferentes métodos para manejar la lógica de traducción y la impresión de resultados. Haciendo que el código sea fácil de entender y mantener.
+
+Manejo de la Entrada:
+
+He implementado un manejo de entrada para asegurarme de que la entrada del usuario sea válida. Verifico si la entrada contiene caracteres alfabéticos o si ya está en código Morse, y proporciono diferentes mensajes de error si la entrada no cumple con los criterios.
+
+Traducción de Palabras a Código Morse:
+
+Para traducir palabras a código Morse, itero sobre cada letra de la palabra ingresada y busco su equivalente en código Morse en el arreglo alphabetLetters. Luego, imprimo el código Morse correspondiente, manteniendo la estructura correcta (espacios entre letras y dos espacios entre palabras).
+
+Traducción de Código Morse a Palabras:
+
+Para traducir código Morse a palabras, divido el código Morse ingresado por el usuario en letras individuales y busco su equivalente en el arreglo morseCode. Luego, imprimo la letra correspondiente del alfabeto, manteniendo la estructura correcta (espacios entre palabras).
+
+\--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+This is a program I have created with the aim of converting words between the alphabet and Morse code, and vice versa.
+
+Code Structure:
+
+I have structured the code into different methods to handle the translation logic and result printing, making the code easy to understand and maintain.
+
+Input Handling:
+
+I have implemented input handling to ensure that user input is valid. I check if the input contains alphabetical characters or if it is already in Morse code, and provide different error messages if the input does not meet the criteria.
+
+Translation from Words to Morse Code:
+
+To translate words to Morse code, I iterate over each letter of the input word and look for its Morse code equivalent in the alphabetLetters array. Then, I print the corresponding Morse code, maintaining the correct structure (spaces between letters and two spaces between words).
+
+Translation from Morse Code to Words:
+
+To translate Morse code to words, I split the Morse code input provided by the user into individual letters and look for their equivalent in the morseCode array. Then, I print the corresponding alphabet letter, maintaining the correct structure (spaces between words).
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,84 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main{
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        char[] alphabetLetters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '};
+
+        String[] morseCode = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+                ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-",
+                ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", " "};
+
+        // Code logic: alphabetLetters[0] == A <==> morseCode[0] == ".-" => A == ".-"
+
+        String userWord = scanner.nextLine();   //Palabra que ingresa el usuario
+
+        int [] result = new int[userWord.length()];
+
+        if ( !userWord.isEmpty() && userWord.matches("[a-zA-Z ]+") ) {
+            String upperCaseWord = userWord.toUpperCase();
+            char[] userWordArray = upperCaseWord.toCharArray();
+
+            for (int i = 0; i < userWordArray.length; i++){         //Recorre el arreglo userWordArray
+                for (int j = 0; j < alphabetLetters.length; j++) {  //Recorre el arreglo alphabetLetters
+                    if (userWordArray[i] == alphabetLetters[j]) {
+                        result[i] = j;
+                        break;
+                    }
+                }
+            }
+            printCodeMorse(result,morseCode);
+        }
+        else if (!userWord.isEmpty() && userWord.matches("[\\.\\- ]+")){
+            List<Integer> positions = new ArrayList<>();
+            String[] userLetters = userWord.split(" ");             //userWord pasa a ser un array de strings separando cada simbolo Morse
+
+            for (int i = 0; i < userLetters.length; i++) {
+                for (int j = 0; j <morseCode.length; j++){
+                    if (userLetters[i].equals(morseCode[j])){
+                        positions.add(j);
+                    }
+                }
+                if (userLetters[i].equals("")){         //Entre palabras completas se agrega -1 como marcador de espacios
+                    positions.add(-1);
+                }
+            }
+
+            printAlphabet(positions,alphabetLetters);
+        }
+        else if ( userWord.isEmpty() ){
+            System.out.println("No ingresaste ninguna palabra");
+        }
+        else {
+            System.out.println("Los caracteres ingresados NO estan permitidos");
+        }
+
+
+        scanner.close();
+    }
+    public static void printCodeMorse(int[] result, String[] morseCode ){
+        for (int indexRes : result){
+            if (morseCode[indexRes] .equals(" ")){                    //En codigo Morse, entre palabras debe haber 2 espacios
+                System.out.print(morseCode[indexRes]);
+            }
+            else {
+                System.out.print(morseCode[indexRes] + " ");    //En codigo Morse, entre cada "letra" debe haber 1 espacio
+            }
+        }
+    }
+
+    public static void printAlphabet(List<Integer> positions, char[] alphabetLetters ){
+        for (int pos : positions){
+            if (pos == -1){
+                System.out.print(" ");
+            }
+            else {
+                System.out.print(alphabetLetters[pos]);
+            }
         }
     }
 }
